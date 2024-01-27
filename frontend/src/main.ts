67eac9601bc969d9a1cc91c32498196dbcfc7814
rin <rin@devne.co>
2024-01-27 19:40:40 +0900

    elementplusの追加

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from "element-plus";
+import '@/../node_modules/element-plus/dist/index.css';
+
 const app = createApp(App)
 
 app.use(router)
+
+app.use(ElementPlus);
 
 app.mount('#app')
